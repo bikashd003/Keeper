2bc5e4713ddb213729e3a6620a11c1de7ab21594
Bikash Das <bikashd003@gmail.com>
2023-03-28 23:33:40 +0530

    Updates

---
+++ precache-manifest.f0f9449497e5e0136bfcef70bf4f43c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae84e90a40100f41620e",
+    "url": "/keeper/static/js/main.ae84e90a.chunk.js"
+  },
+  {
+    "revision": "88823357913e320d05b2",
+    "url": "/keeper/static/js/1.88823357.chunk.js"
+  },
+  {
+    "revision": "503a75061105400542f5",
+    "url": "/keeper/static/js/runtime~main.503a7506.js"
+  },
+  {
+    "revision": "3fb9a0f7fc2233308966d46e081517be",
+    "url": "/keeper/index.html"
+  }
+];
